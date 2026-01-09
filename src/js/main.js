be634411bc7ec4761f7f1715cfcdf7b561660125
Ostap201164 <dyshleviyostap@gmail.com>
2026-01-09 22:18:11 +0200

    merge branches

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,10 @@
-import '../scss/style.scss';
+import "../scss/style.scss";
 
+import { swiper1, swiper2} from "./swiper.js";
+console.log(window.innerWidth);
+
+swiper2();
+
+if (window.innerWidth > 1023) {
+  swiper1();
+}
