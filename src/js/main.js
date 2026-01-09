6c7c2b5454610cf7faafb260ed64ffc88e5e9224
Ostap201164 <dyshleviyostap@gmail.com>
2026-01-09 03:20:44 +0200

    Revert "Merge branch 'main' of https://github.com/Ostap201164/clinic-mecare into services"
    This reverts commit 0d1add6d3765c92c7794fc1fa080a33da1b1042f, reversing
    changes made to 4bb8507f38bb9a7af4d48050787bbee2256b13c3.

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,1 @@
-import "../scss/style.scss";
-
-import { swiper1, swiper2 } from "./swipers.js";
-console.log(window.innerWidth);
-
-swiper2();
-
-if (window.innerWidth > 1023) {
-  swiper1();
-}
+import '../scss/style.scss';
